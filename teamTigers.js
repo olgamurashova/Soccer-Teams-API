d632ca4c9c161b3320c17a0d57a7472b3aedd38d
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-16 11:01:42 -0500

    Add files via upload

---
+++ teamTigers.js
@@ -0,0 +1,89 @@
+//list of team players
+const teamTigers = {
+    players :{
+        '1': {
+            name: 'Brian Malkom',
+            age: 31,
+            position: 'Goalkeeper',
+            goals: 0
+        },
+        '2': {
+            name: 'George Anton',
+            age: 33,
+            position: 'Defender/Back',
+            goals: 17
+        },
+        '3': {
+            name: 'Michael Kors',
+            age: 31,
+            position: 'Defender/Back',
+            goals: 12
+        },
+        '4': {
+            name: 'Tim Wilson',
+            age: 29,
+            position: 'Defender/Back',
+            goals: 11
+        },
+        '5': {
+            name: 'Klim Douglas',
+            age: 29,
+            position: 'Defender/Back',
+            goals: 7
+        },
+        '6': {
+            name: 'Cleo Ostin',
+            age: 33,
+            position: 'Midfielder',
+            goals: 11
+        },
+        '7': {
+            name: 'Alice Cooper',
+            age: 36,
+            position: 'Midfielder',
+            goals: 10
+        },
+        '8': {
+            name: 'Jose Dias',
+            age: 32,
+            position: 'Midfielder',
+            goals: 9
+        },
+        '9': {
+            name: 'Oleg Kudrashov',
+            age: 34,
+            position: 'Striker',
+            goals: 23
+        },
+        '10': {
+            name: 'Isaak Levy',
+            age: 31,
+            position: 'Striker',
+            goals: 15
+        },
+        '11': {
+            name: 'Mike Kipling',
+            age: 27,
+            position: 'Striker',
+            goals: 13
+        },
+        '12': {
+            name: 'Steve Google',
+            age: 31,
+            title: 'Substitute',
+            goals: 1
+        },
+        '13': {
+            name: 'Chris Will',
+            age: 24,
+            position: 'Substitute',
+            goals: 4
+        },
+        '14': {
+            name: 'David Crowley',
+            age: 27,
+            position: 'Substitute',
+            goals: 3
+        }
+    }
+}
