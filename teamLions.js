a4111c5671dfe8701fb7e2b10476335c0c5f2152
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-02-16 11:01:06 -0500

    Add files via upload

---
+++ teamLions.js
@@ -0,0 +1,89 @@
+// list of team players
+const teamLions = {
+    players :{
+        '1': {
+            name: 'Steve Madden',
+            age: 23,
+            position: 'Goalkeeper',
+            goals: 0
+        },
+        '2': {
+            name: 'Harry Anton',
+            age: 32,
+            position: 'Defender/Back',
+            goals: 17
+        },
+        '3': {
+            name: 'Louis Vuitton',
+            age: 31,
+            position: 'Defender/Back',
+            goals: 12
+        },
+        '4': {
+            name: 'Charles Willey',
+            age: 30,
+            position: 'Defender/Back',
+            goals: 11
+        },
+        '5': {
+            name: 'Kim Kessey',
+            age: 29,
+            position: 'Defender/Back',
+            goals: 7
+        },
+        '6': {
+            name: 'Brad Darton',
+            age: 31,
+            position: 'Midfielder',
+            goals: 11
+        },
+        '7': {
+            name: 'Carey Cooper',
+            age: 34,
+            position: 'Midfielder',
+            goals: 10
+        },
+        '8': {
+            name: 'Jessie Doodley',
+            age: 31,
+            position: 'Midfielder',
+            goals: 7
+        },
+        '9': {
+            name: 'Oleg Murashov',
+            age: 34,
+            position: 'Striker',
+            goals: 25
+        },
+        '10': {
+            name: 'Chester Bennington',
+            age: 36,
+            position: 'Striker',
+            goals: 18
+        },
+        '11': {
+            name: 'Mike Shinoda',
+            age: 29,
+            position: 'Striker',
+            goals: 21
+        },
+        '12': {
+            name: 'Brian Architect',
+            age: 31,
+            title: 'Substitute',
+            goals: 1
+        },
+        '13': {
+            name: 'Will Dorns',
+            age: 21,
+            position: 'Substitute',
+            goals: 2
+        },
+        '14': {
+            name: 'Tim Snooty',
+            age: 25,
+            position: 'Substitute',
+            goals: 3
+        }
+    }
+}
